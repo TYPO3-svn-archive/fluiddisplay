4005a94b2039eea49d939ae54fd70140ad3e3a63
fab1en <fab1en@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-14 17:23:54 +0000

    [BUG] fluiddisplay: Make Fluid Display work with TYPO3 4.5, resolves #12234
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fluiddisplay/trunk@42130 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_fluiddisplay.php
+++ class.tx_fluiddisplay.php
@@ -1,29 +1,30 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2007 Francois Suter (Cobweb) <typo3@cobweb.ch>
-*  (c) 2008 Fabien Udriot <fabien.udriot@ecodev.ch>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-*
-* $Id$
-***************************************************************/
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2007 Francois Suter (Cobweb) <typo3@cobweb.ch>
+ *  (c) 2008 Fabien Udriot <fabien.udriot@ecodev.ch>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * $Id$
+ * ************************************************************* */
 
 require_once(t3lib_extMgm::extPath('tesseract', 'services/class.tx_tesseract_feconsumerbase.php'));
 
@@ -46,14 +47,13 @@ class tx_fluiddisplay extends tx_tesseract_feconsumerbase {
 	protected $result = ''; // The result of the processing by the Data Consumer
 	protected $counter = array();
 
-
 	/**
 	 * This method resets values for a number of properties
 	 * This is necessary because services are managed as singletons
 	 *
 	 * @return	void
 	 */
-	public function reset(){
+	public function reset() {
 		$this->structure = array();
 		$this->result = '';
 		$this->uid = '';
@@ -173,108 +173,43 @@ class tx_fluiddisplay extends tx_tesseract_feconsumerbase {
 	 */
 	public function startProcess() {
 
-		// ************************************
-		// ********** INITIALISATION **********
-		// ************************************
-
-		// Initializes local cObj
-		$this->localCObj = t3lib_div::makeInstance('tslib_cObj');
-		$this->configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extKey]);
-
-		$uniqueMarkers = array();
-
-		// ***************************************
-		// ********** BEGINS PROCESSING **********
-		// ***************************************
-
-		// LOCAL DOCUMENTATION:
-		// $templateCode -> HTML template roughly extracted from the database
-		// $templateContent -> HTML that is going to be outputed
-
 		// Loads the template file
-		$templateCode = $this->consumerData['template'];
 
-		if (preg_match('/^FILE:/isU', $templateCode)) {
-			$filePath = str_replace('FILE:', '' , $templateCode);
+		if (preg_match('/^FILE:/isU', $this->consumerData['template'])) {
+			$filePath = str_replace('FILE:', '', $this->consumerData['template']);
 			$filePath = t3lib_div::getFileAbsFileName($filePath);
-			if (is_file($filePath)) {
-				$templateCode = file_get_contents($filePath);
-			}
 		}
-        $templateParser = Tx_Fluid_Compatibility_TemplateParserBuilder::build();
-		$objectManager = t3lib_div::makeInstance('Tx_Fluid_Compatibility_ObjectManager');
-
-		if (isset($GLOBALS['_GET']['debug']['structure']) && isset($GLOBALS['TYPO3_MISC']['microtime_BE_USER_start'])) {
-			t3lib_div::debug($this->structure);
+		else {
+			$filePath = t3lib_div::getFileAbsFileName($this->consumerData['template']);
 		}
 
-        $templateContent = $templateCode;
-        if ($templateContent !== false) {
-			$content = $templateParser->parse($templateContent);
+		if (is_file($filePath)) {
 
-			$variableContainer = $objectManager->create('Tx_Fluid_Core_ViewHelper_TemplateVariableContainer', $this->structure);
-			$renderingContext = $objectManager->create('Tx_Fluid_Core_Rendering_RenderingContext');
-			$renderingContext->setTemplateVariableContainer($variableContainer);
-			$viewHelperVariableContainer = $objectManager->create('Tx_Fluid_Core_ViewHelper_ViewHelperVariableContainer');
-			$renderingContext->setViewHelperVariableContainer($viewHelperVariableContainer);
-			$data = $content->render($renderingContext);
+			$view = t3lib_div::makeInstance('Tx_Fluid_View_StandaloneView');
+			$view->setTemplatePathAndFilename($filePath);
+			$view->assign('datastructure', $this->structure);
+			$this->result = $view->render();
 
-			
 			// Hook that enables to post process the output)
 			if (preg_match_all('/#{3}HOOK\.(.+)#{3}/isU', $this->result, $matches, PREG_SET_ORDER)) {
 				foreach ($matches as $match) {
 					$hookName = $match[1];
 					if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$this->extKey]['postProcessResult'][$hookName])) {
-						foreach($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$this->extKey]['postProcessResult'][$hookName] as $className) {
+						foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$this->extKey]['postProcessResult'][$hookName] as $className) {
 							$postProcessor = &t3lib_div::getUserObj($className);
 							$this->result = $postProcessor->postProcessResult($this->result, $hookName, $this);
 						}
 					}
 				}
 			}
-            $this->result = $data;
-
-
-        }
-
-		
-	}
-
-	/**
-	 * Extracts the filename of a path
-	 *
-	 * @param	string	$filename
-	 * @return	string	the filename
-	 */
-	protected function getFileName($filepath) {
-		$filename = '';
-		$fileInfo = t3lib_div::split_fileref($filepath);
-		if (isset($fileInfo['filebody'])) {
-			$filename = $fileInfo['filebody'];
 		}
-		return $filename;
-	}
-
-	/**
-	 * Displays in the frontend or in the devlog some debug output
-	 *
-	 * @return void
-	 */
-	protected function debug() {
-
-		if (isset($GLOBALS['_GET']['debug']['structure']) && $GLOBALS['TSFE']->beUserLogin) {
-			t3lib_div::debug($this->getDataStructure());
-		}
-
-		if (isset($GLOBALS['_GET']['debug']['filter']) && $GLOBALS['TSFE']->beUserLogin) {
-			t3lib_div::debug($this->getFilter());
+		else {
+			throw new tx_tesseract_exception('No template file has been found in Fluid Display', 1295025186);
 		}
 	}
-
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fluiddisplay/class.tx_fluiddisplay.php']){
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fluiddisplay/class.tx_fluiddisplay.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fluiddisplay/class.tx_fluiddisplay.php']);
 }
-
 ?>
