e4d9607301868ba3c81b9c6e7030dcc6e35fe567
fab1en <fab1en@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-21 03:04:39 +0000

    fluiddisplay: updated ext_emconf after release on TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fluiddisplay/trunk@45321 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "fluiddisplay"
+# Extension Manager/Repository config file for ext "fluiddisplay".
 #
-# Auto generated 23-07-2008 13:29
+# Auto generated 21-03-2011 10:03
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Francois Suter (Cobweb) / Fabien Udriot',
 	'author_email' => 'typo3@cobweb.ch',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'tesseract',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.8',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-0.0.0',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"8077";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d43a";s:14:"ext_tables.php";s:4:"4be3";s:14:"ext_tables.sql";s:4:"14fc";s:36:"icon_tx_fluiddisplay_displays.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"b5c6";s:7:"tca.php";s:4:"ebc7";s:19:"doc/wizard_form.dat";s:4:"b285";s:20:"doc/wizard_form.html";s:4:"25bc";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"8d21";s:10:"README.txt";s:4:"5078";s:25:"class.tx_fluiddisplay.php";s:4:"b9b9";s:12:"ext_icon.gif";s:4:"a406";s:17:"ext_localconf.php";s:4:"909d";s:14:"ext_tables.php";s:4:"da88";s:14:"ext_tables.sql";s:4:"9c07";s:46:"Configuration/TCA/tx_fluiddisplay_displays.php";s:4:"23d1";s:67:"Resources/Private/Language/locallang_csh_txfluiddisplaydisplays.xml";s:4:"a27f";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"afb1";s:50:"Resources/Public/Icons/add_fluiddisplay_wizard.png";s:4:"9cf0";s:51:"Resources/Public/Icons/tx_fluiddisplay_displays.png";s:4:"cd04";s:38:"Samples/class.tx_fluiddisplay_hook.php";s:4:"9c66";s:21:"Samples/locallang.xml";s:4:"cdf6";s:27:"Samples/sampleTemplate.html";s:4:"ca31";s:35:"Samples/Partials/samplePartial.html";s:4:"3bef";s:14:"doc/manual.pdf";s:4:"c1a8";s:14:"doc/manual.sxw";s:4:"02f6";}',
+	'suggests' => array(
+	),
 );
 
 ?>
