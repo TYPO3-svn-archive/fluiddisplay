4f952966ffae8b13f220eb85bb43c2f197a2ef2f
fab1en <fab1en@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-10 09:53:07 +0000

    Update version number in ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fluiddisplay/trunk@64344 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fluiddisplay".
 #
-# Auto generated 21-03-2011 10:03
+# Auto generated 10-07-2012 11:51
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.7.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"8d21";s:10:"README.txt";s:4:"5078";s:25:"class.tx_fluiddisplay.php";s:4:"b9b9";s:12:"ext_icon.gif";s:4:"a406";s:17:"ext_localconf.php";s:4:"909d";s:14:"ext_tables.php";s:4:"da88";s:14:"ext_tables.sql";s:4:"9c07";s:46:"Configuration/TCA/tx_fluiddisplay_displays.php";s:4:"23d1";s:67:"Resources/Private/Language/locallang_csh_txfluiddisplaydisplays.xml";s:4:"a27f";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"afb1";s:50:"Resources/Public/Icons/add_fluiddisplay_wizard.png";s:4:"9cf0";s:51:"Resources/Public/Icons/tx_fluiddisplay_displays.png";s:4:"cd04";s:38:"Samples/class.tx_fluiddisplay_hook.php";s:4:"9c66";s:21:"Samples/locallang.xml";s:4:"cdf6";s:27:"Samples/sampleTemplate.html";s:4:"ca31";s:35:"Samples/Partials/samplePartial.html";s:4:"3bef";s:14:"doc/manual.pdf";s:4:"c1a8";s:14:"doc/manual.sxw";s:4:"02f6";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"7d2c";s:10:"README.txt";s:4:"5078";s:25:"class.tx_fluiddisplay.php";s:4:"e712";s:12:"ext_icon.gif";s:4:"a406";s:17:"ext_localconf.php";s:4:"909d";s:14:"ext_tables.php";s:4:"eaf9";s:14:"ext_tables.sql";s:4:"9c07";s:46:"Configuration/TCA/tx_fluiddisplay_displays.php";s:4:"23d1";s:67:"Resources/Private/Language/locallang_csh_txfluiddisplaydisplays.xml";s:4:"a27f";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"afb1";s:50:"Resources/Public/Icons/add_fluiddisplay_wizard.png";s:4:"9cf0";s:51:"Resources/Public/Icons/tx_fluiddisplay_displays.png";s:4:"cd04";s:38:"Samples/class.tx_fluiddisplay_hook.php";s:4:"9c66";s:21:"Samples/locallang.xml";s:4:"cdf6";s:27:"Samples/sampleTemplate.html";s:4:"ca31";s:35:"Samples/Partials/samplePartial.html";s:4:"3bef";s:14:"doc/manual.pdf";s:4:"c1a8";s:14:"doc/manual.sxw";s:4:"02f6";}',
 	'suggests' => array(
 	),
 );
